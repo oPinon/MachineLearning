907be88a416f0ddcacdf606484bbd108fee2bd9c
oPinon <olivier.pinon.mail@gmail.com>
2016-04-13 15:42:03 -0400

    Finding digits in an image

--- src/MNIST.h
+++ src/MNIST.h
@@ -26,7 +26,7 @@ int readInt(std::fstream& src) {
 }
 
 // returns the index of the class with the highest probability
-int maxProb(const std::vector<double> probs) {
+int maxProb(const std::vector<double>& probs) {
 
 	double bestProb = 0; int bestClass;
 	for (int i = 0; i < probs.size(); i++) {
@@ -91,12 +91,12 @@ void learnMNIST(std::string imagesFileName, std::string labelsFileName) {
 	}
 	// learning the dataset
 	std::random_shuffle(samples.begin(), samples.end());
-	int learnSize = 1000;//(samples.size() * 80) / 100;
+	int learnSize = (samples.size() * 80) / 100;
 	std::vector<Sample> learningSamples(samples.begin(), samples.begin() + learnSize);
-	NetLearner classifier(Network({ nbRows*nbColumns, 300, 10 }));
+	NetLearner classifier(Network({ nbRows*nbColumns, 10 }, 0));
 
 	while (true) {
-		classifier.learn(learningSamples);
+		classifier.learn(learningSamples, 1, 0);
 
 #include <opencv2\opencv.hpp>
 
@@ -123,20 +123,108 @@ void learnMNIST(std::string imagesFileName, std::string labelsFileName) {
 
 		std::cout << (errors * 100) / (testSize) << "% errors on the test set" << std::endl;
 
-		// display the net coeffs for each class (in colum)
-		cv::Mat coeffsViz(cv::Size(nbColumns, 10 * nbRows), CV_64FC1);
+		// display the net coeffs for each class (in a row)
+		cv::Mat coeffsViz;
 		double* coeffsP = (double*)classifier.net.synapses[0].coefficients.data();
+		std::vector<cv::Mat> coeffs;
 		for (int i = 0; i < 10; i++) {
+			cv::Mat coeff(cv::Size(nbColumns, nbRows), CV_64FC1);
 			std::copy( // dont forget the bias
 				coeffsP + i + i*(nbRows*nbColumns),
 				coeffsP + i + (i + 1)*(nbRows*nbColumns),
-				((double*)coeffsViz.data) + i*nbColumns*nbRows
-				);
+				((double*)coeff.data)
+			);
+			coeffs.push_back(coeff);
 		}
+		cv::hconcat(coeffs, coeffsViz);
 		cv::normalize(coeffsViz, coeffsViz, -1, 1, cv::NORM_MINMAX);
 		coeffsViz = 128 * coeffsViz + 128; coeffsViz.convertTo(coeffsViz, CV_8U);
 		cv::applyColorMap(coeffsViz, coeffsViz, cv::COLORMAP_BONE);
-		cv::resize(coeffsViz, coeffsViz, cv::Size(3 * nbColumns, 3 * nbRows * 10), 0, 0, cv::INTER_NEAREST);
-		cv::imshow("coeffs", coeffsViz); cv::waitKey(16);
+		cv::resize(coeffsViz, coeffsViz, cv::Size(10 * 4 * nbColumns, 4 * nbRows), 0, 0, cv::INTER_NEAREST);
+		cv::imshow("coeffs", coeffsViz);
+		if (cv::waitKey(16) == 27) { break; };
+	}
+
+	// find digits in an image (TODO : multiscale)
+	{
+		// reading the source image
+		std::string imPath = "../../data/digits.png";
+		cv::Mat src = cv::imread(imPath);
+		if (src.empty()) { std::cerr << "cant read " << imPath << std::endl; return; }
+		cv::cvtColor(src, src, cv::COLOR_RGB2GRAY);
+		cv::imshow("image", src); cv::waitKey(16);
+		int w = src.size().width, h = src.size().height;
+
+		// where to stock the results of the classifier
+		struct Result {
+			int foundClass;
+			double confidence; // the higher, the better
+		};
+		std::vector<Result> results((w - nbColumns)*(h - nbRows)); // per pixel
+
+		// Convolving the classifier with the image
+		cv::Mat segmentation(cv::Size(w - nbColumns, h - nbRows), CV_64FC1);
+		double* segP = (double*)segmentation.data;
+		for (int y = 0; y < h - nbRows; y++) {
+			for (int x = 0; x < w - nbColumns; x++) {
+
+				// input patch
+				std::vector<double> input(nbRows*nbColumns);
+				for (int y2 = 0; y2 < nbRows; y2++) {
+					for (int x2 = 0; x2 < nbColumns; x2++) {
+						input[y2*nbColumns + x2] =
+							src.data[((y+y2)*w+x+x2)*src.channels()] / 255.0;
+					}
+				}
+
+				// output classes
+				std::vector<double> classes = classifier.apply(input);
+				segP[y*(w - nbColumns) + x] = classes[3];
+				int bestClass = maxProb(classes);
+				results[y*(w - nbColumns) + x] = { bestClass, classes[bestClass] };
+			}
+		}
+		segmentation.convertTo(segmentation, CV_8U, 255);
+		cv::applyColorMap(segmentation, segmentation, cv::COLORMAP_BONE);
+		cv::imshow("probabilities of 3", segmentation); cv::waitKey(16);
+
+		// finding the maximums in the result image
+		double bestConfidence;
+		cv::cvtColor(src, src, cv::COLOR_GRAY2RGB);
+		do {
+			int bestX, bestY, bestClass;
+			bestConfidence = 0;
+
+			// searching for the best result among all pixels
+			for (int y = 0; y < h - nbRows; y++) {
+				for (int x = 0; x < w - nbColumns; x++) {
+					Result& r = results[y*(w - nbColumns) + x];
+					double confidence = r.confidence;
+					if (confidence > bestConfidence) {
+						bestConfidence = confidence;
+						bestX = x;
+						bestY = y;
+						bestClass = r.foundClass;
+					}
+				}
+			}
+
+			// Displaying the result
+			cv::rectangle(src, cv::Rect(bestX, bestY, nbColumns, nbRows), { 0,255,0 });
+			std::stringstream ss; ss << bestClass;
+				//<< " : " << int(100 * bestConfidence) << "%";
+			cv::putText(src, ss.str(), cv::Point(bestX - 12, bestY + nbRows - 1),
+				cv::FONT_HERSHEY_PLAIN, 1, { 0,255,0 });
+
+			// Removing neighboring results (they might be the same digit)
+			for (int y = max(0, bestY - nbRows); y < min(h - nbRows, bestY + nbRows); y++) {
+				for (int x = max(0, bestX - nbColumns); x < min(w - nbColumns, bestX + nbColumns); x++) {
+					results[y*(w - nbColumns) + x].confidence = 0;
+				}
+			}
+		} while (bestConfidence > 0.99); // threshold the results
+
+		cv::imshow("Digits found", src); cv::waitKey(16);
+		cv::waitKey();
 	}
 }
