17ce07e8823237d4153a0550aeba8614a8e728f2
oPinon <olivier.pinon.mail@gmail.com>
2016-04-14 11:44:39 -0400

    Learning faces from the Anefian db
    TODO : remove redundancy by making generic functions for binary export
    and net visualisation

--- src/Faces.cpp
+++ src/Faces.cpp
@@ -91,11 +91,11 @@ void faceTest(std::string folder) {
 	}
 
 	// learning the images
-	int principalComponents = 3;
-	NetLearner learner(Network({ w*h, principalComponents, w*h }, 1));
+	int principalComponents = 8;
+	NetLearner learner(Network({ w*h, principalComponents, w*h }, 0.01));
 
 	while (true) {
-		learner.learn(samples, 1, 32, 1);
+		learner.learn(samples, 1, 32, 0.1);
 
 		// display the net coeffs for each class (in a row)
 		cv::Mat coeffsViz;
@@ -129,17 +129,22 @@ void faceTest(std::string folder) {
 		cv::resize(original, original, cv::Size(256,256));
 		cv::resize(output, output, cv::Size(256,256));
 		cv::imshow("src", original); cv::imshow("dst", output);
-		cv::waitKey();
+		if (cv::waitKey() == 27) { break; }
 	}
 #endif
 
 	// detecting faces in an image
 	{
+		// reading the image
 		cv::Mat src = cv::imread("../../data/kid.png");
 		cv::cvtColor(src, src, cv::COLOR_RGB2GRAY);
 		cv::imshow("face to detect", src); cv::waitKey(16);
 		src.convertTo(src, CV_64FC1); src = src / 255;
-		for (float scale = 1; src.size().width * scale > w && src.size().height > h; scale *= 0.8) {
+
+		// for different scales
+		for (float scale = 1; int(src.size().width * scale) > w && int(src.size().height) > h; scale *= 0.8) {
+			
+			// scaling the image
 			cv::Mat srcSmall;
 			cv::resize(
 				src, srcSmall,
@@ -150,9 +155,12 @@ void faceTest(std::string folder) {
 			cv::Mat dst(cv::Size(wI - w, hI - h), CV_64FC1);
 			double* dstP = (double*)dst.data;
 
+			// for each region of the image
 			for (int y = 0; y < hI - h; y++) {
 				for (int x = 0; x < wI - w; x++) {
-					cv::Mat patch(srcSmall(cv::Rect(x, y, w, h)));
+
+					cv::Mat patch(cv::Size(w, h), CV_64FC1);
+					srcSmall(cv::Rect(x, y, w, h)).copyTo(patch);
 					cv::normalize(patch, patch, 0, 1, cv::NORM_MINMAX);
 					std::vector<double> input((double*)patch.data, (double*)patch.data + w*h);
 					std::vector<double> output = learner.apply(input);
@@ -161,7 +169,7 @@ void faceTest(std::string folder) {
 						double diff = input[k] - output[k];
 						error += abs(diff);
 					}
-					dstP[y*(wI - w) + x] = error;
+					dstP[y*(wI - w) + x] = exp(-error/(w*h));
 				}
 			}
 			cv::normalize(dst, dst, -1, 1, cv::NORM_MINMAX);
@@ -176,42 +184,175 @@ void faceTest(std::string folder) {
 // http://www.anefian.com/research/GTDB_README.txt
 void faceTest2(std::string folder) {
 
-	// for each person
-	for (int i = 1; i <= 50; i++) {
-		std::stringstream ss;
-		ss << 100 + i;
-		std::string personFolder = "s" + ss.str().substr(1) + "/",
-			imFolder = folder + "gt_db/" + personFolder;
+	int wF = 32, hF = 32; // faces dimensions
+	std::vector<Sample> samples;
 
-		// for each image of the person
-		for (int j = 1; j <= 15; j++) {
-			std::stringstream ss2;
-			ss2 << 100 + j;
-			std::string imPath = imFolder + ss2.str().substr(1) + ".jpg";
-
-			// reading the image
-			cv::Mat im = cv::imread(imPath);
-			if (im.empty()) {
-				std::cerr << "can't read " << imPath << std::endl;
-				return;
-			}
-
-			// reading the label
-			std::stringstream ss3;
-			ss3 << 1000 + 20 * (i - 1) + j;
-			std::string labelPath = folder + "labels/" +
-				+"lab" + ss3.str().substr(1);
-			std::fstream label(labelPath, std::ios::in);
-			if (!label.is_open()) {
-			std:cerr << "can't read " << labelPath << std::endl;
-				return;
-			}
-			int x, y, w, h;
-			label >> x >> y >> w >> h;
-
-			cv::rectangle(im, { x,y,w - x,h - y }, { 0,255,0 });
-
-			cv::imshow("k", im); cv::waitKey();
+	std::string binPath = folder + "allSamples";
+	std::fstream binSamples(binPath, std::ios::in | std::ios::binary);
+	if (binSamples.is_open()) {
+		int nb; binSamples.read((char*)&nb, sizeof(int));
+		std::cout << "importing " << nb << " samples from " << binPath << std::endl;
+		for (int i = 0; i < nb; i++) {
+			std::vector<unsigned char> pixels(wF*hF);
+			binSamples.read((char*)pixels.data(), wF*hF*sizeof(unsigned char));
+			unsigned char label; binSamples.read((char*)&label, sizeof(unsigned char));
+			Sample s;
+			s.input = std::vector<double>(wF*hF);
+			for (int i = 0; i < wF*hF; i++) { s.input[i] = pixels[i] / 255.0; }
+			s.output = { label / 255.0 };
+			samples.push_back(s);
 		}
+	}
+	else {
+		// for each person
+		for (int i = 1; i <= 50; i++) {
+			std::stringstream ss;
+			ss << 100 + i;
+			std::string personFolder = "s" + ss.str().substr(1) + "/",
+				imFolder = folder + "gt_db/" + personFolder;
+
+			// for each image of the person
+			for (int j = 1; j <= 15; j++) {
+				std::stringstream ss2;
+				ss2 << 100 + j;
+				std::string imPath = imFolder + ss2.str().substr(1) + ".jpg";
+
+				// reading the image
+				cv::Mat im = cv::imread(imPath);
+				if (im.empty()) {
+					std::cerr << "can't read " << imPath << std::endl;
+					return;
+				}
+
+				// reading the label
+				std::stringstream ss3;
+				ss3 << 1000 + 20 * (i - 1) + j;
+				std::string labelPath = folder + "labels/" +
+					+"lab" + ss3.str().substr(1);
+				std::fstream label(labelPath, std::ios::in);
+				if (!label.is_open()) {
+				std:cerr << "can't read " << labelPath << std::endl;
+					return;
+				}
+				int x, y, x2, y2;
+				label >> x >> y >> x2 >> y2;
+
+				// Getting the face
+				int w = x2 - x, h = y2 - y;
+				cv::Mat face(cv::Size(w, h), CV_8UC3);
+				{
+					im(cv::Rect(x, y, w, h)).copyTo(face);
+
+					// downscaling it (TODO : check ratio)
+					double ratio = min(double(w) / wF, double(h) / hF);
+					cv::resize(face, face, cv::Size(w / ratio, h / ratio), 0, 0, cv::INTER_AREA);
+
+					int w = face.size().width, h = face.size().height;
+					cv::Mat cropped(cv::Size(wF, hF), CV_8UC1);
+					face(cv::Rect(0, h - hF, wF, hF)).copyTo(cropped);
+					//cv::imshow("z", cropped); cv::waitKey();
+					cv::cvtColor(cropped, cropped, cv::COLOR_RGB2GRAY);
+
+					Sample s;
+					s.input = std::vector<double>(wF*hF);
+					for (int i = 0; i < wF*hF; i++) { s.input[i] = cropped.data[i] / 255.0; }
+					s.output = { 1.0 }; // 1 because it is a face
+					samples.push_back(s);
+
+					cv::Mat imS;
+					cv::resize(im, imS, cv::Size(im.size().width / ratio, im.size().height / ratio), 0, 0, cv::INTER_AREA);
+					cv::cvtColor(imS, imS, cv::COLOR_RGB2GRAY);
+
+					// extracting a non-face image
+					int xS = x / ratio, yS = y / ratio;
+					bool found = false;
+					while (!found) {
+						int xRand = (imS.size().width - wF) * double(rand()) / RAND_MAX;
+						int yRand = (imS.size().height - hF) * double(rand()) / RAND_MAX;
+
+						// if to close from face, discard it
+						if ((xRand - xS)*(xRand - xS) + (yRand - yS)*(yRand - yS) < wF*hF / 16) { continue; }
+
+						found = true;
+						cv::Mat nonFace;
+						im(cv::Rect(xRand, yRand, wF, hF)).copyTo(nonFace);
+						cv::cvtColor(nonFace, nonFace, cv::COLOR_RGB2GRAY);
+
+						Sample s;
+						s.input = std::vector<double>(wF*hF);
+						for (int i = 0; i < wF*hF; i++) { s.input[i] = nonFace.data[i] / 255.0; }
+						s.output = { 0 };
+						samples.push_back(s);
+					}
+				}
+
+				//cv::rectangle(im, { x,y,w,h }, { 0,255,0 });
+				//cv::imshow("k", im); cv::waitKey(1);
+			}
+		}
+
+		// exporting to binary file
+		binSamples = std::fstream(binPath, std::ios::out | std::ios::binary);
+		if (!binSamples.is_open()) { std::cerr << "can't write to " << binPath << std::endl; }
+		else {
+			int nb = samples.size();
+			binSamples.write((char*)&nb, sizeof(int));
+			for (const Sample& s : samples) {
+				std::vector<unsigned char> pixels(wF*hF);
+				for (int i = 0; i < wF*hF; i++) { pixels[i] = s.input[i] * 255; }
+				binSamples.write((char*)pixels.data(), wF*hF*sizeof(unsigned char));
+				unsigned char label = s.output[0] * 255;
+				binSamples.write((char*)&label, sizeof(unsigned char));
+			}
+		}
+	}
+
+#if 0
+	// displaying the samples
+	for (const Sample& s : samples) {
+		cv::Mat face(cv::Size(wF,hF),CV_8UC1);
+		for (int i = 0; i < wF*hF; i++) { face.data[i] = s.input[i] * 255; }
+		cv::resize(face, face, cv::Size(8 * wF, 8 * hF), 0, 0, cv::INTER_NEAREST);
+		cv::imshow(s.output[0] > 0.5 ? "Face" : "Non-Face", face); cv::waitKey();
+	}
+#endif
+
+	int nbComponents = 10;
+	NetLearner classifier(Network({ wF*hF, nbComponents, 1 }, 0.001));
+
+	int nbToLearn = 80 * samples.size() / 100;
+	//std::random_shuffle(samples.begin(), samples.end());
+	std::vector<Sample>& learningSamples = std::vector<Sample>(samples.begin(),samples.begin()+nbToLearn);
+	std::vector<Sample>& testingSamples = std::vector<Sample>(samples.begin() + nbToLearn,samples.end());
+	
+	while (true) {
+		classifier.learn(learningSamples, 1, 8);
+		int error = 0;
+		for (Sample& s : testingSamples) {
+			double result = classifier.apply(s.input)[0];
+			if ((result < 0.5) != (s.output[0] < 0.5)) { error++; }
+		}
+		std::cout << "error on Test is " << (error*100.0) / testingSamples.size() << "%" << std::endl;
+
+		// TODO : generic function to display the coeffs of a network
+		cv::Mat coeffsViz;
+		double* coeffsP = (double*)classifier.net.synapses[0].coefficients.data();
+		std::vector<cv::Mat> coeffs;
+		for (int i = 0; i < nbComponents; i++) {
+			cv::Mat coeff(cv::Size(wF, hF), CV_64FC1);
+			std::copy( // dont forget the bias
+				coeffsP + i + i*(wF*hF),
+				coeffsP + i + (i + 1)*(wF*hF),
+				((double*)coeff.data)
+				);
+			coeffs.push_back(coeff);
+		}
+		cv::hconcat(coeffs, coeffsViz);
+		cv::normalize(coeffsViz, coeffsViz, -1, 1, cv::NORM_MINMAX);
+		coeffsViz = 128 * coeffsViz + 128; coeffsViz.convertTo(coeffsViz, CV_8U);
+		cv::applyColorMap(coeffsViz, coeffsViz, cv::COLORMAP_BONE);
+		cv::resize(coeffsViz, coeffsViz, cv::Size(nbComponents * 4 * wF, 4 * hF), 0, 0, cv::INTER_NEAREST);
+		cv::imshow("coeffs", coeffsViz);
+		if (cv::waitKey(16) == 27) { break; };
 	}
 }
